c06a06ebbc1c8ea7dd4a7fbd36418ae048be6781
Daniil211212 <daniilrodionov621@gmail.com>
2024-06-13 17:32:38 +0300

    new version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import Add from "./components/add";
 import Header from "./components/header";
 import Table from "./components/table";
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Table />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Table />} />
+          <Route path='add' element={<Add />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,12 +1,22 @@
 import styles from '../styles/table.module.css';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Table = () => {
+    
+    const navigate = useNavigate();
+
+    const HandleAdd = () => {
+      navigate('/add');
+    };
+
     return(
         <body>
             <div className={styles.cont} >
                 <div className={styles.get}>
                     <p>Список записей</p>
-                    <button>Добавить</button>
+                    <button className={styles.button} onClick={HandleAdd}>Добавить</button>
                 </div>
             </div>
         </body>
