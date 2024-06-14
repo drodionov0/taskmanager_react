0884d2f441e7f1a89a529c9f41c08f97034866de
Daniil211212 <daniilrodionov621@gmail.com>
2024-06-14 14:49:53 +0300

    new version

--- src/components/add.js
+++ src/components/add.js
@@ -1,9 +1,19 @@
+import styles from '../styles/add.module.css'
+
 
 const Add = () => {
     return(
         <body>
-            <div>
-                s
+            <div className={styles.container}>
+                <div className={styles.formadd}>
+                    <form className={styles.formad}>
+                        <label className={styles.lab}>Наименование</label>
+                        <input className={styles.inp}/>
+                        <label className={styles.lab}>Описание</label>
+                        <textarea className={styles.inp2}/>
+                        <label className={styles.lab}>Тип задачи</label>
+                    </form>
+                </div>
             </div>
         </body>
     )
