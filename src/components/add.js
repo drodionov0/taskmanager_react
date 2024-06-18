a018664edae65903752f669e051b02811e36cd70
Daniil211212 <daniilrodionov621@gmail.com>
2024-06-18 15:11:23 +0300

    new version

--- src/components/add.js
+++ src/components/add.js
@@ -1,7 +1,21 @@
 import styles from '../styles/add.module.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 
 const Add = () => {
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/tasks')
+        .then(response => {
+            setTasks(response.data);
+        })
+        .catch(error => console.log(error))
+    }, [])
+
     return(
         <body>
             <div className={styles.container}>
@@ -12,6 +26,11 @@ const Add = () => {
                         <label className={styles.lab}>Описание</label>
                         <textarea className={styles.inp2}/>
                         <label className={styles.lab}>Тип задачи</label>
+                        <select>
+                            {tasks.map(task => (
+                                <option key={task.id}>{task.type}</option>
+                            ))}
+                        </select>
                     </form>
                 </div>
             </div>
