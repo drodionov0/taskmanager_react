934de6dab629452c5663c49adaad407c4c7bad43
Daniil211212 <daniilrodionov621@gmail.com>
2024-06-27 13:40:56 +0300

    new version

--- src/components/table.js
+++ src/components/table.js
@@ -1,11 +1,41 @@
 import styles from '../styles/table.module.css';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+
+const MAX_PER_PAGE = 5;
 
 
 const Table = () => {
     
+    const [currentPage, setCurrentPage] = useState(1);
+    const [task, setTask] = useState([]);
+
     const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/task')
+        .then(response => {
+            setTask(response.data)
+        })
+        .catch(error => console.log(error))
+    },[])
+
+    const handlePageClick = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const getPaginatedData = () => {
+        const startIndex = (currentPage - 1) * MAX_PER_PAGE;
+        const endIndex = startIndex + MAX_PER_PAGE;
+        return task.slice(startIndex, endIndex);
+    };
+
+    const getPageNumbers = () => {
+        const totalPages = Math.ceil(task.length / MAX_PER_PAGE);
+        return Array.from({ length: totalPages }, (_, index) => index + 1);
+    };
 
     const HandleAdd = () =>  {
       navigate('/add');
@@ -30,7 +60,30 @@ const Table = () => {
                                 <th>Дата изменения</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            {getPaginatedData().map(task => (
+                                <tr key={task.id} className={styles.comp}>
+                                    <td>{task.id}</td>
+                                    <td>{task.nameTask}</td>
+                                    <td>{task.typeTask}</td>
+                                    <td>{task.nameInit}</td>
+                                    <td>{task.dateInit}</td>
+                                    <td>{task.dateRed}</td>
+                                </tr>
+                            ))}
+                        </tbody>
                     </table>
+                </div>
+                <div className={styles.pagination}>
+                    {getPageNumbers().map(number => (
+                        <button
+                            key={number}
+                            onClick={() => handlePageClick(number)}
+                            className={number === currentPage ? styles.active : ''}
+                        >
+                            {number}
+                        </button>
+                    ))}
                 </div>
             </div>
         </body>
