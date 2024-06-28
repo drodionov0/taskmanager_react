88ba7f917cd44b3cf4c932cf26576d8b4228c2f0
Daniil211212 <daniilrodionov621@gmail.com>
2024-06-28 17:23:07 +0300

    new version

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -1,9 +1,19 @@
-import Header from "./header";
 import styles from '../styles/edit.module.css';
 
 const EditTask = () => {
-    return(
-        <Header />
+    return( 
+        <div className={styles.container}>
+            <div className={styles.formadd}>
+                <form className={styles.formad}>
+                    <label className={styles.lab}>Наименование</label>
+                    <input className={styles.inp} />
+                    <label className={styles.lab}>Описание</label>
+                    <textarea className={styles.inp2} />
+                    <label className={styles.lab}>Тип</label>
+                    <select className={styles.sel} />
+                </form>
+            </div>
+        </div>
     )
 }
 
